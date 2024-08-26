1650131f12804f233f77d75d8c3ef3f93fed5a7b
phoenix-coder <amrutapanchal.014@gmail.com>
2024-08-27 02:55:48 +0530

    Contract with token

--- contracts/Crowd.sol
+++ contracts/Crowd.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CrowdFunding{
+/*contract CrowdFunding{
     address public owner;
     uint public rid =1;
     struct Request {
@@ -62,6 +62,111 @@ contract CrowdFunding{
 
         requests[id].raisedAmount += msg.value;
         requests[id].receiver.transfer(msg.value);
+    }
+
+}*/
+
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 decimal
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, decimal);
+    }
+}
+
+contract CrowdFunding{
+    address public owner;
+    uint public rid =1;
+    Token public crowdToken; 
+    
+    struct Request {
+        uint id;
+        string title;
+        string description;
+        uint deadline;
+        uint target;
+        uint raisedAmount;
+        bool approved;
+        address payable receiver;
+    }
+
+   
+    mapping (uint => Request) public requests;
+    event showOndashboard (uint id, string title, string description, uint deadline, uint target, uint raisedAmount, bool approved, address receiver);
+    mapping (address => uint) public showToken;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function register (string memory _title, string memory _description, uint _deadline, uint _target) public {
+        Request memory newRequest = Request({
+            id: rid,
+            title: _title,
+            description : _description,
+            deadline : block.timestamp + (_deadline * 60),
+            target : _target,
+            approved : false,
+            receiver : payable (msg.sender),
+            raisedAmount : 0
+        });
+        
+        requests[rid] = newRequest;
+        rid +=1;
+
+    }
+
+    function generateToken (string memory name, string memory symbol, uint256 decimal) public {
+        require(msg.sender == owner,"Only owner");
+        crowdToken = new Token(name, symbol, decimal);
+        showToken[msg.sender] = decimal;
+    }
+
+    function approveToken(uint amount) public returns (uint){
+        require(msg.sender == owner, "Only owner can approve tokens.");
+        crowdToken.approve(address(this), amount);
+        return crowdToken.allowance(msg.sender, address(this));
+    }
+
+    function approveRequest(uint id) public {
+        require(msg.sender == owner,"Only owner can approve request");
+        Request memory myRequest = requests[id];
+
+        require(block.timestamp < myRequest.deadline, "Deadline is over");
+        require(requests[id].receiver != address(0), "Invalid receiver. Please add valid reciever");
+
+        requests[id].approved = true;
+
+        emit showOndashboard(myRequest.id, myRequest.title, myRequest.description, myRequest.deadline, myRequest.target, myRequest.raisedAmount, myRequest.approved, myRequest.receiver);
+    }
+
+    function donate(uint id) public payable {
+        
+        require(requests[id].approved == true, "Invalid request Id");
+        require(block.timestamp < requests[id].deadline, "Deadline is over");
+        require(requests[id].receiver != address(0), "Invalid receiver. Please add valid reciever");
+        require(requests[id].target > requests[id].raisedAmount, "Already satisfied");
+        require(msg.value > 0, "Amount must be greater than 0");
+
+        //uint val = msg.value*2/100;
+        
+        requests[id].raisedAmount += msg.value;
+        requests[id].receiver.transfer(msg.value);
+
+        uint256 tokenAmount = 2; 
+        require(crowdToken.allowance(owner, address(this)) >= tokenAmount, "CrowdFunding contract not approved to transfer tokens.");
+
+   
+        crowdToken.transferFrom(owner, msg.sender, tokenAmount);
+
+        showToken[owner] -= 2;
+        showToken[msg.sender] += 2;
     }
 
 }

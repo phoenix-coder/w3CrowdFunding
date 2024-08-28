632a58a40969b5d6e44b4c1256461466781f3bfe
phoenix-coder <amrutapanchal.014@gmail.com>
2024-08-28 21:24:07 +0530

    Working contract

--- contracts/Crowd.sol
+++ contracts/Crowd.sol
@@ -1,94 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/*contract CrowdFunding{
-    address public owner;
-    uint public rid =1;
-    struct Request {
-        uint id;
-        string title;
-        string description;
-        uint deadline;
-        uint target;
-        uint raisedAmount;
-        bool approved;
-        address payable receiver;
-    }
-
-   
-    mapping (uint => Request) public requests;
-    event showOndashboard (uint id, string title, string description, uint deadline, uint target, uint raisedAmount, bool approved, address receiver);
-
-    constructor(){
-        owner = msg.sender;
-    }
-
-    function register (string memory _title, string memory _description, uint _deadline, uint _target) public {
-        Request memory newRequest = Request({
-            id: rid,
-            title: _title,
-            description : _description,
-            deadline : block.timestamp + (_deadline * 60),
-            target : _target,
-            approved : false,
-            receiver : payable (msg.sender),
-            raisedAmount : 0
-        });
-        
-        requests[rid] = newRequest;
-        rid +=1;
-
-    }
-
-    function approve(uint id) public {
-        require(msg.sender == owner,"Only owner can approve request");
-        Request memory myRequest = requests[id];
-
-        require(block.timestamp < myRequest.deadline, "Deadline is over");
-        require(requests[id].receiver != address(0), "Invalid receiver. Please add valid reciever");
-
-        requests[id].approved = true;
-
-        emit showOndashboard(myRequest.id, myRequest.title, myRequest.description, myRequest.deadline, myRequest.target, myRequest.raisedAmount, myRequest.approved, myRequest.receiver);
-    }
-
-    function donate(uint id) public payable {
-        
-        require(requests[id].approved == true, "Invalid request Id");
-        require(block.timestamp < requests[id].deadline, "Deadline is over");
-        require(requests[id].receiver != address(0), "Invalid receiver. Please add valid reciever");
-        require(requests[id].target > requests[id].raisedAmount, "Already satisfied");
-        require(msg.value > 0, "Amount must be greater than 0");
-
-        requests[id].raisedAmount += msg.value;
-        requests[id].receiver.transfer(msg.value);
-    }
-
-}*/
-
-
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     constructor(
         string memory name,
         string memory symbol,
-        uint256 decimal
+        uint256 totalSupply
     ) ERC20(name, symbol) {
-        _mint(msg.sender, decimal);
+        _mint(msg.sender, totalSupply);
     }
 }
 
 contract CrowdFunding{
-    address public owner;
-    uint public rid =1;
+    address owner;
+    uint rid =1;
     Token public crowdToken; 
     
     struct Request {
         uint id;
-        string title;
-        string description;
+        bytes title;
+        bytes description;
         uint deadline;
         uint target;
         uint raisedAmount;
@@ -96,10 +29,8 @@ contract CrowdFunding{
         address payable receiver;
     }
 
-   
     mapping (uint => Request) public requests;
-    event showOndashboard (uint id, string title, string description, uint deadline, uint target, uint raisedAmount, bool approved, address receiver);
-    mapping (address => uint) public showToken;
+    event showOndashboard (uint id, bytes title, bytes description, uint deadline, uint target, uint raisedAmount, bool approved, address receiver);
 
     constructor(){
         owner = msg.sender;
@@ -108,8 +39,8 @@ contract CrowdFunding{
     function register (string memory _title, string memory _description, uint _deadline, uint _target) public {
         Request memory newRequest = Request({
             id: rid,
-            title: _title,
-            description : _description,
+            title: bytes(_title),
+            description : bytes(_description),
             deadline : block.timestamp + (_deadline * 60),
             target : _target,
             approved : false,
@@ -122,16 +53,9 @@ contract CrowdFunding{
 
     }
 
-    function generateToken (string memory name, string memory symbol, uint256 decimal) public {
+    function generateToken (string memory name, string memory symbol, uint256 totalSupply) public{
         require(msg.sender == owner,"Only owner");
-        crowdToken = new Token(name, symbol, decimal);
-        showToken[msg.sender] = decimal;
-    }
-
-    function approveToken(uint amount) public returns (uint){
-        require(msg.sender == owner, "Only owner can approve tokens.");
-        crowdToken.approve(address(this), amount);
-        return crowdToken.allowance(msg.sender, address(this));
+        crowdToken = new Token(name, symbol, totalSupply);
     }
 
     function approveRequest(uint id) public {
@@ -146,7 +70,7 @@ contract CrowdFunding{
         emit showOndashboard(myRequest.id, myRequest.title, myRequest.description, myRequest.deadline, myRequest.target, myRequest.raisedAmount, myRequest.approved, myRequest.receiver);
     }
 
-    function donate(uint id) public payable {
+    function donate(uint id) public payable returns (uint){
         
         require(requests[id].approved == true, "Invalid request Id");
         require(block.timestamp < requests[id].deadline, "Deadline is over");
@@ -160,13 +84,10 @@ contract CrowdFunding{
         requests[id].receiver.transfer(msg.value);
 
         uint256 tokenAmount = 2; 
-        require(crowdToken.allowance(owner, address(this)) >= tokenAmount, "CrowdFunding contract not approved to transfer tokens.");
+        require(crowdToken.balanceOf(address(this)) >= tokenAmount, "Not enough tokens in contract");
 
-   
-        crowdToken.transferFrom(owner, msg.sender, tokenAmount);
-
-        showToken[owner] -= 2;
-        showToken[msg.sender] += 2;
+        crowdToken.transfer(msg.sender, tokenAmount);
+        return crowdToken.balanceOf(msg.sender);
     }
 
 }
